49c07b31e5e4a88dc5de82f30991608d41a0616b
dafalagi <dafarizky34@gmail.com>
2024-12-19 11:37:25 +0800

    feat: remove iCal link processing from villa update logic

--- app/Http/Controllers/VillasController.php
+++ app/Http/Controllers/VillasController.php
@@ -961,7 +961,6 @@ class VillasController extends Controller
             'cor_lat' => $request->cor_lat,
             'cor_long' => $request->cor_long,
             'status' => $statusPost,
-            'link_ical' => $request->link_ical,
         );
 
         $villa = Villas::findOrFail($id);
@@ -1665,78 +1664,6 @@ class VillasController extends Controller
             ];
             Pricing::where('id', $request->pricing_id)->update($pricing);
         }
-        if ($request->link_ical != null) {
-            $fileContents = file_get_contents($request->link_ical);
-            $cek = preg_split("/\n|\r\n/", $fileContents);
-            $search = array_search("BEGIN:VEVENT",$cek,true);
-            for ($i=1; $i < $search ; $i++) { 
-                unset($cek[$i]);
-            }
-            $data_filter = implode(',',$cek);
-            // dd($data_filter);
-            $text = [
-                "BEGIN:VCALENDAR", 
-                ",BEGIN:VEVENT", 
-                ",END:VEVENT,END:VCALENDAR,",
-                ",END:VEVENT,END:VCALENDAR",
-                ",END:VEVENT",
-                ",DTEND;VALUE=DATE:",
-                ",DTSTART;VALUE=DATE:",
-                ",SUMMARY:",
-                ",UID:",
-                ",UUID:",
-                ",DESCRIPTION:",
-            ];
-            $array   = [
-                '[', 
-                '{"text":"',
-                '"}]', 
-                '"}]', 
-                '"},',
-                ',end_date:',
-                ',start_date:',
-                ',summary:',
-                ',uuid:',
-                ',uuid:',
-                ',description:'
-            ];
-            
-            $data = trim(preg_replace('/\s+/', '', $data_filter));
-            $newPhrase = str_replace($text, $array, $data);
-            $json = json_decode($newPhrase);
-            // dd($json);
-            foreach ($json as $key => $value) {
-                $explode1 = explode(',',$value->text);
-                $result[$key]['uuid'] = "";
-                $result[$key]['start_date'] = "";
-                $result[$key]['end_date'] = "";
-                $result[$key]['description'] = "";
-                $result[$key]['summary'] = "";
-                $result[$key]['villa_id'] = $id;
-                $result[$key]['created_at'] = now();
-                $result[$key]['updated_at'] = now();
-                foreach ($explode1 as $key1 => $value1) {
-                    $explode2 = explode(':',$value1);
-                    if($explode2[0]=='description'){
-                        $result[$key][$explode2[0]] = preg_replace('/\s+/', '',str_replace('description:', '', $value1));
-                    }elseif($explode2[0]=='start_date'){
-                        $result[$key][$explode2[0]] = date_create_from_format("Ymd", substr($explode2[1],0,8));
-                    }elseif($explode2[0]=='end_date'){
-                        $result[$key][$explode2[0]] = date_create_from_format("Ymd", substr($explode2[1],0,8));
-                    }elseif($explode2[0]=='uuid'){
-                        $result[$key][$explode2[0]] = $explode2[1];
-                    }elseif($explode2[0]=='summary'){
-                        $result[$key][$explode2[0]] = $explode2[1];
-                    }else{
-
-                    }
-                }
-            }
-            // dd($result);
-            DB::table('calenders')->where('villa_id', $id)->delete();
-            Calender::insert($result);
-        }
-
 
         return redirect()->route('admin.villa.edit', $id)
             ->with(['notif_status' => '1', 'notif' => 'Update data succeed.']);
