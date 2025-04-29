1726afde2831b6a780b36b62db097d9d18181cab
kiel888897 <kiel888897@gmail.com>
2025-04-29 16:27:47 +0800

    Cannot use object of type stdClass as array

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 
 class CalendarController extends Controller
 {
-    
+
     public function index()
     {
         $data["page_title"] = 'Manajemen Facilities';
@@ -18,42 +18,42 @@ class CalendarController extends Controller
 
     public function import(Request $request, $id)
     {
-        if(empty($request->ical_link) and empty($request->file('ical'))) {
+        if (empty($request->ical_link) and empty($request->file('ical'))) {
             return redirect()->route('admin.villa.edit', ['id' => $id])
                 ->with(['notif_status' => '0', 'notif' => 'Ical link or file required']);
         }
 
-        if(!empty($request->ical_link)) {
+        if (!empty($request->ical_link)) {
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_URL, $request->ical_link);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0');
-            
+
             $fileContents = curl_exec($ch);
 
             curl_close($ch);
-        }else {
+        } else {
             $file = $request->file('ical');
             $fileContents = file_get_contents($file);
         }
 
-        if(empty($fileContents)) {
+        if (empty($fileContents)) {
             return redirect()->route('admin.villa.edit', ['id' => $id])
                 ->with(['notif_status' => '0', 'notif' => 'Import failed because empty']);
         }
-        
+
         try {
             $cek = preg_split("/\n|\r\n/", $fileContents);
-            $search = array_search("BEGIN:VEVENT",$cek,true);
-            for ($i=1; $i < $search ; $i++) { 
+            $search = array_search("BEGIN:VEVENT", $cek, true);
+            for ($i = 1; $i < $search; $i++) {
                 unset($cek[$i]);
             }
-            $data_filter = implode(',',$cek);
+            $data_filter = implode(',', $cek);
 
             $text = [
-                "BEGIN:VCALENDAR", 
-                ",BEGIN:VEVENT", 
+                "BEGIN:VCALENDAR",
+                ",BEGIN:VEVENT",
                 ",END:VEVENT,END:VCALENDAR,",
                 ",END:VEVENT,END:VCALENDAR",
                 ",END:VEVENT",
@@ -67,10 +67,10 @@ class CalendarController extends Controller
                 ",DTEND:",
             ];
             $array   = [
-                '[', 
+                '[',
                 '{"text":"',
-                '"}]', 
-                '"}]', 
+                '"}]',
+                '"}]',
                 '"},',
                 ',end_date:',
                 ',start_date:',
@@ -81,12 +81,12 @@ class CalendarController extends Controller
                 ',start_date:',
                 ',end_date:',
             ];
-            
+
             $data = trim(preg_replace('/\s+/', '', $data_filter));
             $newPhrase = str_replace($text, $array, $data);
             $json = json_decode($newPhrase);
 
-            if(empty($json) and !empty($newPhrase)){
+            if (empty($json) and !empty($newPhrase)) {
                 $newPhrase = str_replace("[{", "", $newPhrase);
                 $newPhrase = str_replace("}]", "", $newPhrase);
                 $newPhrase = str_replace('"text":",', "", $newPhrase);
@@ -94,30 +94,33 @@ class CalendarController extends Controller
                 $newPhrase = explode('},{', $newPhrase);
 
                 $json = [];
-                foreach($newPhrase as $value) {
+                foreach ($newPhrase as $value) {
                     $json[] = (object) [
                         'text' => $value
                     ];
                 }
             }
 
-            if(empty($json)) {
+            if (empty($json)) {
                 return redirect()->route('admin.villa.edit', ['id' => $id])
                     ->with(['notif_status' => '0', 'notif' => 'Import failed because empty']);
             }
 
-            if(!str_contains($json[0]->text, 'start_date')) {
+            if (!str_contains($json[0]->text, 'start_date')) {
                 return redirect()->route('admin.villa.edit', ['id' => $id])
                     ->with(['notif_status' => '0', 'notif' => 'Import failed because start date not found']);
             }
 
-            if(!str_contains($json[0]->text, 'end_date')) {
+            if (!str_contains($json[0]->text, 'end_date')) {
                 return redirect()->route('admin.villa.edit', ['id' => $id])
                     ->with(['notif_status' => '0', 'notif' => 'Import failed because end date not found']);
             }
 
             foreach ($json as $key => $value) {
-                $explode1 = explode(',',$value->text);
+                if (!is_object($value) || !property_exists($value, 'text')) {
+                    continue; // Lewatkan jika bukan objek atau tidak ada "text"
+                }
+                $explode1 = explode(',', $value->text);
                 $result[$key]['uuid'] = "";
                 $result[$key]['start_date'] = "";
                 $result[$key]['end_date'] = "";
@@ -128,21 +131,21 @@ class CalendarController extends Controller
                 $result[$key]['updated_at'] = now();
 
                 foreach ($explode1 as $key1 => $value1) {
-                    $explode2 = explode(':',$value1);
-                    if($explode2[0]=='description'){
-                        $result[$key][$explode2[0]] = preg_replace('/\s+/', '',str_replace('description:', '', $value1));
-                    }elseif($explode2[0]=='start_date'){
-                        $result[$key][$explode2[0]] = date_create_from_format("Ymd", substr($explode2[1],0,8));
-                    }elseif($explode2[0]=='end_date'){
-                        $result[$key][$explode2[0]] = date_create_from_format("Ymd", substr($explode2[1],0,8));
-                    }elseif($explode2[0]=='uuid'){
+                    $explode2 = explode(':', $value1);
+                    if ($explode2[0] == 'description') {
+                        $result[$key][$explode2[0]] = preg_replace('/\s+/', '', str_replace('description:', '', $value1));
+                    } elseif ($explode2[0] == 'start_date') {
+                        $result[$key][$explode2[0]] = date_create_from_format("Ymd", substr($explode2[1], 0, 8));
+                    } elseif ($explode2[0] == 'end_date') {
+                        $result[$key][$explode2[0]] = date_create_from_format("Ymd", substr($explode2[1], 0, 8));
+                    } elseif ($explode2[0] == 'uuid') {
                         $result[$key][$explode2[0]] = $explode2[1];
-                    }elseif($explode2[0]=='summary'){
+                    } elseif ($explode2[0] == 'summary') {
                         $result[$key][$explode2[0]] = $explode2[1];
-                    }elseif(str_contains($value1, 'DTSTART')) {
-                        $result[$key]['start_date'] = date_create_from_format("Ymd", substr($explode2[1],0,8));
-                    }elseif(str_contains($value1, 'DTEND')) {
-                        $result[$key]['end_date'] = date_create_from_format("Ymd", substr($explode2[1],0,8));
+                    } elseif (str_contains($value1, 'DTSTART')) {
+                        $result[$key]['start_date'] = date_create_from_format("Ymd", substr($explode2[1], 0, 8));
+                    } elseif (str_contains($value1, 'DTEND')) {
+                        $result[$key]['end_date'] = date_create_from_format("Ymd", substr($explode2[1], 0, 8));
                     }
                 }
             }
@@ -150,12 +153,12 @@ class CalendarController extends Controller
             DB::table('calenders')->where('villa_id', $id)->delete();
             Calender::insert($result);
 
-            if(!empty($request->ical_link)) {
+            if (!empty($request->ical_link)) {
                 $villa = Villas::find($id);
                 $villa->update([
                     'link_ical' => $request->ical_link
                 ]);
-            }else {
+            } else {
                 $villa = Villas::find($id);
                 $villa->update([
                     'link_ical' => $request->ical_link
@@ -206,6 +209,4 @@ class CalendarController extends Controller
 
         return false;
     }
-
-    
 }
